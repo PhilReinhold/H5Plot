f84ec6e3c386319dd80dad734f78891165472043
Phil Reinhold <pcreinhold@gmail.com>
2013-07-08 17:30:23 -0400

    Change serializer back to pickle, since serpent doesn't like numpy arrays.

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ manager_uri = "PYRO:%s@%s:%d" % (manager_id, manager_host, manager_port)
 RUNNING = True
 
 Pyro4.config.HMAC_KEY = '6551d449b0564585a9d39c0bd327dcf1'
+Pyro4.config.SERIALIZER = 'pickle'
 
 h5file_directory = r'S:\_Data'
 h5file_filter = 'HDF5 Files (*.h5)'
