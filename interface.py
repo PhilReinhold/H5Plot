40a49c1baaaec2de42c2a0c72ac54657ac34ea8b
Phil Reinhold <pcreinhold@gmail.com>
2013-08-13 15:57:44 -0400

    refresh connection to window by default

--- interface.py
+++ interface.py
@@ -3,5 +3,5 @@ import objectsharer as objsh
 def plotwindow_client(serveraddr='127.0.0.1', serverport=55557, localaddr='127.0.0.1'):
     zbe = objsh.ZMQBackend()
     zbe.start_server(addr=localaddr)
-    zbe.connect_to('tcp://%s:%d' % (serveraddr, serverport))     # Data server
+    zbe.refresh_connection('tcp://%s:%d' % (serveraddr, serverport))     # Data server
     return objsh.helper.find_object('plotwin')
