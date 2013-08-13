38bee57796b70afa617e6b93beb318d15a05d459
Phil Reinhold <pcreinhold@gmail.com>
2013-08-13 11:52:03 -0400

    No Pyro anymore

--- config.py
+++ config.py
@@ -1,15 +1,3 @@
-import Pyro4
-
-manager_id = 'manager'
-manager_host = 'localhost'
-manager_port = 5555
-manager_uri = "PYRO:%s@%s:%d" % (manager_id, manager_host, manager_port)
-RUNNING = True
-
-Pyro4.config.HMAC_KEY = '6551d449b0564585a9d39c0bd327dcf1'
-Pyro4.config.SERIALIZER = 'pickle'
-
-#h5file_directory = r'C:\_Data'
-from dataserver_helpers import DATA_DIRECTORY
+from dataserver import DATA_DIRECTORY
 h5file_directory = DATA_DIRECTORY
 h5file_filter = 'HDF5 Files (*.h5)'
